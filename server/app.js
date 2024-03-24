21c9911c085d04f4dd157b47f3cb23c06cc7b4c0
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-24 12:40:51 +0100

    done
    there is a middleware that throws the error and there is also the middle that catches it and displayes the apporpriate message to it.usually the catch all displays an obj, throwing an error also requires setting the status of that error

--- server/app.js
+++ server/app.js
@@ -4,6 +4,18 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
-
+app.use((req,res,next)=> {
+ const err = new Error("Sorry the requested resource couldn't be found");
+ err.status = 404;
+ next(err);
+})
+app.use((err,req,res,next)=> {
+  res.status(err.status || 500).json({
+   
+      message: err.message,
+      statusCode: err.status
+    
+  });
+})
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
